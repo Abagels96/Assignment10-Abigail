603183f53cf8ddf46dd259f8d15bb78ef3fa1fb1
Abigail Bales <138115235+Abagels96@users.noreply.github.com>
2025-05-29 11:19:00 -0500

    fixed optional variables

--- src/main/java/com/coderscampus/Assignment10/service/MealPlannerService.java
+++ src/main/java/com/coderscampus/Assignment10/service/MealPlannerService.java
@@ -2,6 +2,7 @@ package com.coderscampus.Assignment10.service;
 
 import java.net.URI;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -13,12 +14,16 @@ import com.coderscampus.Assignment10.DTO.WeekResponse;
 @Service
 public class MealPlannerService {
 
+	@Value("${api.key}")
+	private String apiKey;
+	
+	
 	public ResponseEntity<DayResponse> callSpoonacularDayApi(String diet, String exclude, String targetCalories) {
 
 		URI uriDay = UriComponentsBuilder.newInstance().scheme("https").host("api.spoonacular.com")
 				.path("/mealplanner/generate").queryParam("timeFrame", "day")
 				.queryParam("targetCalories", targetCalories).queryParam("diet", diet).queryParam("exclude", exclude)
-				.queryParam("apiKey", "26da184289a34f92b8c8562015a8d1d3").build().toUri();
+				.queryParam("apiKey", apiKey).build().toUri();
 
 		RestTemplate rt1 = new RestTemplate();
 
@@ -32,7 +37,7 @@ public class MealPlannerService {
 		URI uriWeek = UriComponentsBuilder.newInstance().scheme("https").host("api.spoonacular.com")
 				.path("/mealplanner/generate").queryParam("timeFrame", "week").queryParam("diet", diet)
 				.queryParam("exclude", exclude).queryParam("targetCalories", targetCalories)
-				.queryParam("apiKey", "26da184289a34f92b8c8562015a8d1d3").build().toUri();
+				.queryParam("apiKey", apiKey).build().toUri();
 		RestTemplate rt1 = new RestTemplate();
 		ResponseEntity<WeekResponse> responseWeek = rt1.getForEntity(uriWeek, WeekResponse.class);
 		System.out.println(responseWeek.getBody());
